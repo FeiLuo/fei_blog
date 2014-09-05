605ac4f33a1e14f3dfb4dee6def5426a0f00fd18
cnluofei <luofeitarget@sina.com>
2014-09-05 17:05:44 +0800

    initial framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeiBlog::Application.config.secret_token = '037799e4b87215410f279d8a484e7242ffcd8aafa6fbd18467a9c229e43f2427f6052aed00a7680575d263d546389bdc068ed5fe1e3c5b3241916226cb76b42b'
